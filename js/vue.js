d30bc80324744780c2928f612b79763671fa3741
JimmyOra <s73980@htw-dresden.de>
2018-02-19 13:16:11 +0100

    Added v0.1

---
+++ js/vue.js
@@ -0,0 +1,14 @@
+var vue= new Vue({
+    el: "#platinput",
+    data: {
+      plattform: "",
+      url: "https://www.facebook.com/"
+    },
+  });
+
+  var telvue= new Vue({
+    el: "#telinput",
+    data: {
+     number:""
+    },
+  });

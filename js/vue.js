3404f1e1a66b0880f1fd8cf55da9b7521833dafe
unknown <Tobias Rudoph>
2018-02-19 17:33:33 +0100

    json einbindung

--- js/vue.js
+++ js/vue.js
@@ -2,7 +2,14 @@ var vue= new Vue({
     el: "#platinput",
     data: {
       plattform: "",
-      url: "https://www.facebook.com/"
+      url: "https://www.facebook.com/",
+      stakeholder: []
+    },
+    created: function(){
+      $.getJSON('static/process.json',function(json){
+        this.stakeholder = json.process.stakeholder;
+        console.log(this.stakeholder);
+      });
     },
   });
 

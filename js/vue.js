70fd8c979471b78e768428e54cb5a033b3a711f2
Tobias Rudolph <thetobi95@gmail.com>
2018-02-20 18:00:26 +0100

    kontakt page

--- js/vue.js
+++ js/vue.js
@@ -1,35 +1,41 @@
 
 
-  var vue= new Vue({
-    el: "#allinput",
-    data: {
-     number:"",
-     stakeholder: []
-    },
-    created: function(){
-      $.getJSON('static/process.json',function(json){
-        vue.$data.stakeholder = json.process.stakeholder;
-      });
-    },
-    filters: {
-      firstname: function(value){
-        if(!value) return '';
-        value=value.toString();
-        var pos = value.lastIndexOf(" ");
-        if(pos===-1)
-          return value;
-        return value.slice(0,pos);
-        
-  
-    },
-    lastname: function(value){
-      if(!value) return '';
-      value=value.toString();
+var vue = new Vue({
+  el: "#allinput",
+  data: {
+    number: "",
+    stakeholder: [],
+    index: 0,
+  },
+  created: function () {
+    $.getJSON('static/process.json', function (json) {
+      vue.$data.stakeholder = json.process.stakeholder;
+    });
+  },
+  filters: {
+    firstname: function (value) {
+      if (!value) return '';
+      value = value.toString();
       var pos = value.lastIndexOf(" ");
-      if(pos===-1)
+      if (pos === -1)
+        return value;
+      return value.slice(0, pos);
+
+
+    },
+    lastname: function (value) {
+      if (!value) return '';
+      value = value.toString();
+      var pos = value.lastIndexOf(" ");
+      if (pos === -1)
         return "";
       return value.slice(pos);
 
+    }
+  },
+  methods: {
+    setid: function (index) {
+      this.index = index;
+    }
   }
-  }
-  });
+});
